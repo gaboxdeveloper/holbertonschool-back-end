2fd5c2c49d9bb75a167218617e73698ee2e1d4bc
Gabriel <grossy.sjb@gmail.com>
2023-12-23 20:22:56 +0000

    task1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -9,22 +9,24 @@ if __name__ == "__main__":
         access to the api
     """
 
-    url = "https://jsonplaceholder.typicode.com"
-    user_id = int(argv[1])
-    user_data = requests.get(url + f'/users/{user_id}').json()
-    user_tasks = requests.get(url + f'/users/{user_id}/todos').json()
+    url = "https://jsonplaceholder.typicode.com/"
+    employee_id = int(argv[1])
+    employee_data = requests.get(url + f'users/{employee_id}').json()
+    employee_tasks = requests.get(url + f'users/{employee_id}/todos').json()
 
-    print(f'Employee {user_data["name"]} is done with tasks:')
-    
-    for task in user_tasks:
+    completed_tasks = [task for task in employee_tasks if task["completed"]]
+
+    print(f'Employee {employee_data["name"]} is done with tasks:')
+
+    for task in completed_tasks:
         print(f'\t[{"" if task["completed"] else "not "}completed] {task["title"]}')
 
-    csv_filename = f'{user_id}.csv'
+    csv_filename = f'{employee_id}.csv'
     with open(csv_filename, mode='w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
-        for task in user_tasks:
-            csv_writer.writerow([user_id, user_data["name"], task["completed"], task["title"]])
+        for task in employee_tasks:
+            csv_writer.writerow([employee_id, employee_data["name"], task["completed"], task["title"]])
 
     print(f'Data exported to {csv_filename}')
